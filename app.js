dba59b0e200aa834d7efc749d861a06961f0873b
Dearie <eburujacintha@gmail.com>
2023-09-07 15:42:29 +0100

    Corrected the app file

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-const user = JSON.parse(
-    fs.readFileSync(`${__dirname}/data.json`)
-)
+const user = JSON.parse(fs.readFileSync(`${__dirname}/data.json`));
+
+const currentDate = new Date();
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentDayIndex = currentDate.getUTCDay();
+const currentDayName = dayNames[currentDayIndex];
+
+const currentUTCTime = new Date().toISOString();
 
 const getUser = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: user.length,
-        data: {
-            user
-        }
-    });
+  res.status(200).json({
+    status: "success",
+    results: user.length,
+    data: {
+      slack_name: req.query.slack_name,
+      current_day: currentDayName,
+      utc_time: currentUTCTime,
+      track: req.query.track,
+      github_file_url: user.github_file_url,
+      github_repo_url: user.github_repo_url,
+      status_code: user.status_code,
+    },
+  });
 };
 
-app.get('/api/v1/user', getUser);
+app.get("/api/v1/user", getUser);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
