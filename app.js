b7c3fb923c7ea9709747cd860b02f104d836accd
Dearie <eburujacintha@gmail.com>
2023-09-09 11:24:39 +0100

    changed

--- app.js
+++ app.js
@@ -22,21 +22,19 @@ const currentDayName = dayNames[currentDayIndex];
 
 const currentUTCTime = new Date().toISOString();
 
-const getUser = (req, res) => {
 
+
+app.get("/api", (req,res)=> {
   res.status(200).json({
-      slack_name: req.query.slack_name,
-      current_day: currentDayName,
-      utc_time: currentUTCTime,
-      track: req.query.track,
-      github_file_url: user.github_file_url,
-      github_repo_url: user.github_repo_url,
-      status_code: user.status_code,
-  });
- 
-};
-
-app.get("/api", getUser);
+    slack_name: req.query.slack_name,
+    current_day: currentDayName,
+    utc_time: currentUTCTime,
+    track: req.query.track,
+    github_file_url: user.github_file_url,
+    github_repo_url: user.github_repo_url,
+    status_code: user.status_code,
+});
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
