1cab7296466017040400a3c933ff9c5820226a44
Dearie <eburujacintha@gmail.com>
2023-09-09 13:24:12 +0100

    fixed again

--- app.js
+++ app.js
@@ -19,27 +19,19 @@ const dayNames = [
 ];
 const currentDayIndex = currentDate.getUTCDay();
 const currentDayName = dayNames[currentDayIndex];
+const currentUtcTime = new Date().toISOString();
 
-// const current = new Date();
-// const currentUTCTime = current.toISOString().split('.')[0] + 'Z';
-
-// const currentUTCTime = new Date().toISOString().slice(0, 19) + 'Z';
-
-// let utc = new Date().toISOString().slice(0, 19) + "Z";
 
 app.get("/api", (req, res) => {
-  const utc = new Date(
-    new Date().getTime() + new Date().getTimezoneOffset() * 60000
-  );
-  const utc_time = utc.toISOString().split(".")[0] + "Z";
+ 
   res.status(200).json({
     slack_name: req.query.slack_name,
     current_day: currentDayName,
-    utc_time: utc.toISOString().split(".")[0] + "Z",
+    utc_time: currentUtcTime,
     track: req.query.track,
     github_file_url: user.github_file_url,
     github_repo_url: user.github_repo_url,
-    status_code: user.status_code,
+    status_code: user.status_code, 
   });
 });
 const port = 3000;
